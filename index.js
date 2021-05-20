82113586c48303dacdf7a2ac087b9b4bb6d02f99
Abhishek <abhi.ghosh27feb@gmail.com>
2021-05-20 21:20:49 +0530

    add

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-var mongoose = require('mongoose')
-var menu
-var Menus
+const express = require('express')
+const mongoose=require('mongoose')
 connectToDb()
-createModel()
-function connectToDb(){
+function connectToDb() {
 
     var uri = "mongodb://abhi:mummum%4027@cluster0-shard-00-00.szwhm.mongodb.net:27017,cluster0-shard-00-01.szwhm.mongodb.net:27017,cluster0-shard-00-02.szwhm.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-n35fii-shard-0&authSource=admin&retryWrites=true&w=majority";
+    //    var uri='mongodb://localhost:27017/restrorent'
+    mongoose.connect(uri);
 
-mongoose.connect(uri);
+    const connection = mongoose.connection;
 
-const connection = mongoose.connection;
+    connection.once("open", function () {
+        console.log("MongoDB database connection established successfully");
+    });
 
-connection.once("open", function() {
-  console.log("MongoDB database connection established successfully");
-});
-    // mongoose.connect('mongodb+srv://abhi:<mummum@27>@cluster0.bua7w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(
-    //     () => console.log("connected to mongodb")).catch(error => {
-    
-    //         console.log("getting error", error)
-    //     })
-    
 }
-function createModel(){
-    const menuSchema = new mongoose.Schema({ name: String, price: String, Description: String, type: String })
-    //model
-    Menus =mongoose.model('Menus', menuSchema)
-    menu = new Menus({ name: 'chickn fry', price: 1000, Description: 'chickn', type: 'Main course' })
-    
-}
-//schema
+// const users =require('./login')
+const app = express()
+app.use(express.json());
+var cors = require('cors')
+app.use(cors())
+// app.use('/api/users',users)
+app.options('*', cors())
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log("listning on port 3000")
+})
+//creating model
+var User=mongoose.model('User',new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:4,
+        maxlength:50
+    },
+    email:{
 
-//save
-function saveDb(){
-    menu.save().then(result=>{
-        console.log("menu data",result)
-    }).catch(error=>{
-        console.log("error",error)
-    })
-}
-//find
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:255,
+        unique:true
+    },
+    password:{
 
-
- function fetchMenu(){
-     var menuArrName=[]
-   Menus.find().then(data=>{
-        console.log("data",data)
-          return data
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:255,
         
-     }).catch(error=>{
-         console.log("error=>",error)
-     })
-    //  console.log("menus",menuArrName)
-    
- 
- }
+    }
+}))
+app.post('/adduser',(req,res)=>{
 
-//  fetchMenu()
-saveDb()
+    user=new User ({name:req.body.name,email:req.body.email,password:req.body.password})
+    user.save().then(res=>{
 
-module.exports.fetchMenu = fetchMenu;
-module.exports.saveDb = saveDb;
-module.exports.createModel = createModel;
-module.exports.connectToDb = connectToDb;
+        console.log("res",res)
+        
+    }).catch(error=>{
+      console.log("error",error)  
+    });
+    res.send(user)
+})
